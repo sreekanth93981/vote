495e2c2e2ae71ba57f0e1ebedacf3591e57599d8
sreekanth93981 <sreekanthtanneeru57@gmail.com>
2024-09-04 09:39:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+
+{
+
+    static void Main()
+
+    {
+
+        int age = 10; // Example age; this could be replaced with dynamic input in a different context
+
+        string result = CheckVoteEligibility(age);
+
+        Console.WriteLine(result);
+
+        static string CheckVoteEligibility(int age)
+
+        {
+
+            if (age >= 18)
+
+            {
+
+                return "You are eligible to vote!";
+
+            }
+
+            else
+
+            {
+
+                return "You are not eligible to vote yet.";
+
+            }
+
+        }
+
+    }
+}
+
+
+
